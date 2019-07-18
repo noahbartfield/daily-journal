35f9a52f30f096611de4ab7001ae743597f0f4d2
Noah Bartfield <noahbartfield@Noahs-MBP.attlocal.net>
2019-07-18 06:51:41 -0500

    Dynamically adding journal entry objects to HTML

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -5,18 +5,13 @@
 
 
 
-const journalEntry = [
-    // {
-    //     date: dateOfEntry,
-    //     conceptsCovered: conceptsCoveredOfEntry,
-    //     entryContent: contentOfEntry,
-    //     mood: moodOfEntry
-    // },
+const journalEntries = [
+
     {
         date: "07/12/2019",
         conceptsCovered: "Objects",
         entryContent: "Today we learned about objects",
-        mood: "Good"
+        mood: "Great!"
     },
     {
         date: "07/11/2019",
@@ -32,34 +27,61 @@ const journalEntry = [
     }
 ]
 
+const allEntries = document.querySelector("#entryLog")
 
-for (let i = 0; i < journalEntry.length; i++) {
-    const entry = journalEntry[i];
-    document.querySelector(".entryLog").innerHTML = 
-    `<h1>${entry.conceptsCovered}</h1>
-    <p>${entry.entryContent}</p>
-    <h4>${entry.date}</h4>
-    <p>${entry.mood}</p>`
 
-    
+const createJournalEntry = function (date, title, content, mood) {
+    return `
+    <section class="journalContainer">
+        <h2>${title}</h2>
+        <p>${content}</p>
+        <p>${date}<p>
+        <h4>Mood: ${mood}</h4>
+    </section>
+    `
 }
 
 
-const allEntries = []
-allEntries.push(journalEntry)
 
 
-// using foreach
-journalEntry.forEach(data => {
-    console.log(data.date, data.conceptsCovered, data.entryContent, data.mood)
+journalEntries.forEach(entry => {
+    let journalEntry = ""
+    journalEntry = createJournalEntry(entry.date, entry.conceptsCovered, entry.entryContent, entry.mood)
+    allEntries.innerHTML += journalEntry
 });
+
+
+
+console.log(allEntries)
+
+
+// for (let i = 0; i < journalEntry.length; i++) {
+//     const entry = journalEntry[i];
+//     document.querySelector("#entryLog").innerHTML = 
+//     `<h1>${entry.conceptsCovered}</h1>
+//     <p>${entry.entryContent}</p>
+//     <h4>${entry.date}</h4>
+//     <p>${entry.mood}</p>`
+
+
+// }
+
+
+// const allEntries = []
+// allEntries.push(journalEntry)
+
+
+// // using foreach
+// journalEntry.forEach(data => {
+//     console.log(data.date, data.conceptsCovered, data.entryContent, data.mood)
+// });
 
 
 // using for loop
 // for (let i = 0; i < journalEntry.length; i++) {
 //     const data = journalEntry[i];
 //     console.log(data.date, data.conceptsCovered, data.entryContent, data.mood)
-    
+
 // }
 
 
@@ -78,6 +100,6 @@ journalEntry.forEach(data => {
 
 // function printToDom (entry) {
 //     entry = document.querySelector("entryLog").innerHTML
-    
+
 //     return(entry)
 // }
